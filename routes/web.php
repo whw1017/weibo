17845f6114f753e2bfca18ed02e2031615fac56f
whw <348644381@qq.com>
2019-03-19 20:08:45 +0800

    添加Love路由

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
+Route::get('/love', 'StaticPagesController@love');
